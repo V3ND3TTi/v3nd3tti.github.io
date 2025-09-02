2bcac930e31c9c9720b82ec6abd9c1da846e52ef
Devon Vendetti <v3nd3tti@icloud.com>
2025-09-01 22:39:32 -0700

    added transition effects to overlay and socials

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
     <div
       id="overlay"
       v-show="showUI"
-      class="absolute bottom-16 md:bottom-32 lg:bottom-48 w-full flex flex-col items-center justify-center text-center text-white z-10 pointer-events-none"
+      class="absolute bottom-16 md:bottom-32 lg:bottom-48 w-full flex flex-col items-center justify-center text-center text-white z-10 pointer-events-none transition-opacity duration-1000 opacity-0"
+      :class="{ 'opacity-100': showUI }"
     >
       <p class="text-md md:text-2xl lg:text-4xl font-light tracking-tighter lg:tracking-wider">
         .NET C# DEV ◾ WEB DEV ◾ GAME DEV
@@ -18,7 +19,8 @@
     <div 
       id="socials"
       v-show="showUI"
-      class="absolute bottom-6 w-full flex justify-center space-x-8 md:space-x-12 text-white z-10"
+      class="absolute bottom-6 w-full flex justify-center space-x-8 md:space-x-12 text-white z-10 transition-opacity duration-1000 opacity-0"
+      :class="{ 'opacity-100': showUI }"
     >
       <a href="https://github.com/v3nd3tti" target="_blank" class="text-2xl md:text-6xl hover:text-blue-400 hover:scale-110 transition-transform duration-200"><i class="fa-brands fa-github"></i></a>
       <a href="https://linkedin.com/in/v3nd3tti" target="_blank" class="text-2xl md:text-6xl hover:text-blue-400 hover:scale-110 transition-transform duration-200"><i class="fa-brands fa-linkedin"></i></a>
