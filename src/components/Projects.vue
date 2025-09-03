db1d608a359d8bfc38df7a3909235b1c9d7c74f7
Devon Vendetti <v3nd3tti@icloud.com>
2025-09-03 06:01:43 -0700

    added coming soon to projects and contact pages

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,5 @@
 <template>
   <section id="projects" class="min-h-screen flex items-center justify-center bg-gray-800 text-white">
-    <h2 class="text-4xl font-bold text-blue-400">Projects</h2>
+    <h2 class="text-4xl font-bold text-blue-400">Projects: Coming Soon!</h2>
   </section>
 </template>
